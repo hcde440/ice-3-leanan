cb6384337810b9aee1adf16cccf2aee61d06474b
leanan <leanan@uw.edu>
2019-04-17 00:05:35 -0700

    Thought I understood what was happening with the display but turns out I dont :( looking up solutions

--- getpressure-LN/getpressure-LN.ino
+++ getpressure-LN/getpressure-LN.ino
@@ -5,6 +5,21 @@
 #include <DHT_U.h>
 #include "config.h"
 
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+
 Adafruit_MPL115A2 mpl115a2;
 
 // set up the 'pressure'feed
@@ -12,6 +27,14 @@ AdafruitIO_Feed *pressure = io.feed("pressure");
 
 void setup(void) 
 {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
   Serial.begin(115200);
   Serial.println("Hello!");
   
@@ -28,10 +51,18 @@ void loop(void)
   
   
   // save pressure to Adafruit IO
-  pressure->save(mpl115a2.getPressure());
+  pressure->save(pressureKPA);
   
   temperatureC = mpl115a2.getTemperature();  
   Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C");
   
   delay(1000);
+
+  display.clearDisplay(); //Clears any existing images or text
+  display.setCursor(0,0); //Puts cursor on top left corner
+    // prints pressure message on display
+  display.print("Pressure: "); 
+  display.print(pressureKPA, 3); 
+  display.println(F(" kPa"));
+  display.display(); //Displaying the display
 }
